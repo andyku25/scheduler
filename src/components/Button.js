dc119364cd815998c3466052c3297dd3f4e29e05
Andrew <andrew.ku93@gmail.com>
2021-03-15 15:41:45 -0400

    complete the button component exercise to add styling and actions depending on cases

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,16 @@
 import React from "react";
+import classNames, { className } from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   const { children , onClick, confirm, danger, disabled } = props;
+   const buttonClass = classNames("button", {
+      "button--confirm": confirm,
+      "button--danger": danger
+   });
+
+   return (
+      <button className={ buttonClass } onClick={ onClick } disabled={ disabled } >{ children }</button>
+   );
 }
