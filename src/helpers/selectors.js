273814370f48358c246fae0a31835221d2c6786f
Andrew <andrew.ku93@gmail.com>
2021-03-17 18:43:07 -0400

    create helper function- selector for getting the appointment objects for a given day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,10 @@
+export function getAppointmentsForDay(state, day) {
+    const appointmentsOnDay = state.days.filter(appointment => (appointment.name === day));
+    if (appointmentsOnDay[0]) {
+      const appointmentsArr = [...appointmentsOnDay[0].appointments];
+      return appointmentsArr.map(id => {
+        return state.appointments[id];
+      })
+    }
+    return [];
+};
