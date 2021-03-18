680d4f74463e7e1df5801fe57c347f558c340df5
Andrew <andrew.ku93@gmail.com>
2021-03-18 12:36:56 -0400

    render appointments for each day dynamically with promises to fetch 'API' data

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -2,9 +2,7 @@ export function getAppointmentsForDay(state, day) {
     const appointmentsOnDay = state.days.filter(appointment => (appointment.name === day));
     if (appointmentsOnDay[0]) {
       const appointmentsArr = [...appointmentsOnDay[0].appointments];
-      return appointmentsArr.map(id => {
-        return state.appointments[id];
-      })
+      return appointmentsArr.map(id => state.appointments[id])
     }
     return [];
 };
